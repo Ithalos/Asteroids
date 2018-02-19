1e948c817c644ebf3b3f162438da3264c6ce54ef
Sam Triest <sam.triest@gmail.com>
2018-02-19 20:51:17 +0100

    Add Vector2 constructor

--- Maths/Vector2.lua
+++ Maths/Vector2.lua
@@ -10,3 +10,15 @@ Vector2 =
     x, y
 }
 
+--[[
+    Creates and returns a new Vector2, after assigning it values for x & y.
+]]
+function Vector2:New(x, y)
+    newVector2 = {}
+    setmetatable(newVector2, self)
+    self.__index = self
+    newVector2.x = x
+    newVector2.y = y
+    return newVector2
+end
+
