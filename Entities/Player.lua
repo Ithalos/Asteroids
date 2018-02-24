a71a1deeea649470ca1b5f999f267e1a988a2e14
Sam Triest <sam.triest@gmail.com>
2018-02-24 20:33:00 +0100

    Add Player data members

---
+++ Entities/Player.lua
@@ -0,0 +1,21 @@
+--[[
+    File: Player.lua
+    Author: Sam Triest
+
+    The Player class defines the blueprint for the player's ship,
+    and its operations.
+]]
+
+Player =
+{
+    mesh,
+    position,
+    rotation,
+    rotationSpeed,
+    velocity,
+    direction,
+    friction,
+    speed,
+    speedCutoff,
+}
+
