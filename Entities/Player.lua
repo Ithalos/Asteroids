ec47db32a4245d8f4620b6655afe959d8c810b1a
Sam Triest <sam.triest@gmail.com>
2018-02-24 20:46:44 +0100

    Add Player constructor

--- Entities/Player.lua
+++ Entities/Player.lua
@@ -19,3 +19,22 @@ Player =
     speedCutoff
 }
 
+--[[
+    Creates and returns a new Player object.
+]]
+function Player:New(mesh, position, rotationSpeed, friction, speed, speedCutoff)
+    newPlayer = {}
+    setmetatable(newPlayer, self)
+    self.__index = self
+    newPlayer.mesh = mesh
+    newPlayer.position = position
+    newPlayer.rotation = 0
+    newPlayer.rotationSpeed = rotationSpeed
+    newPlayer.velocity = Vector2:New(0, 0)
+    newPlayer.direction = Vector2:New(0, 0)
+    newPlayer.friction = friction
+    newPlayer.speed = speed
+    newPlayer.speedCutoff = speedCutoff
+    return newPlayer
+end
+
