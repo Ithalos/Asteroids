4907ff8e4da0d761e6351f9bc908f12f557a4db8
Sam Triest <sam.triest@gmail.com>
2018-03-12 23:44:00 +0100

    Add movement functionality to the player's ship

--- Entities/Player.lua
+++ Entities/Player.lua
@@ -46,6 +46,43 @@ function Player:New(vertices, position)
 end
 
 --[[
+    Moves the player's ship.
+]]
+function Player:Move(dt)
+    self.direction.x = math.cos(self.rotation)
+    self.direction.y = math.sin(self.rotation)
+    self.direction:Normalise()
+
+    if love.keyboard.isDown("left") then
+        self.rotation = self.rotation - self.rotationSpeed * dt
+    elseif love.keyboard.isDown("right") then
+        self.rotation = self.rotation + self.rotationSpeed * dt
+    end
+
+    if love.keyboard.isDown("up") then
+        self.velocity.x = self.velocity.x + self.direction.x * dt
+        self.velocity.y = self.velocity.y + self.direction.y * dt
+    else
+        if self.velocity:Length() > 0 then
+            if self.velocity:Length() < self.speedCutoff then
+                self.velocity.x = 0
+                self.velocity.y = 0
+            else
+                normalised = Normalise(self.velocity)
+                normalised.x = normalised.x * self.friction * dt
+                normalised.y = normalised.y * self.friction * dt
+
+                self.velocity.x = self.velocity.x - normalised.x
+                self.velocity.y = self.velocity.y - normalised.y
+            end
+        end
+    end
+
+    self.position.x = self.position.x + self.velocity.x * self.speed * dt
+    self.position.y = self.position.y + self.velocity.y * self.speed * dt
+end
+
+--[[
     Sets up circle-to-circle collision settings for the player's ship.
     This will hopefully be replaced with a more precise system at a later date.
 ]]
