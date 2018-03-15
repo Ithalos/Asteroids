d64651a176f5577ea2bc2a9826459fb958db6967
Sam Triest <sam.triest@gmail.com>
2018-03-15 22:39:44 +0100

    Add Asteroid data members

---
+++ Entities/Asteroid.lua
@@ -0,0 +1,18 @@
+--[[
+    File: Asteroid.lua
+    Author: Sam Triest
+
+    Defines the blueprint for asteroids, and their operations.
+]]
+
+Asteroid =
+{
+    mesh,
+    position,
+    velocity,
+    direction,
+    speed,
+    size,
+    collisionRadius
+}
+
