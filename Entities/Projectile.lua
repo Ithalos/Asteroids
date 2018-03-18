e79546d2de059bd7fc9ce772e0d52f2b2ddf449b
Sam Triest <sam.triest@gmail.com>
2018-03-18 23:37:02 +0100

    Add Projectile data members

---
+++ Entities/Projectile.lua
@@ -0,0 +1,15 @@
+--[[
+    File: Projectile.lua
+    Author: Sam Triest
+
+    Defines the blueprint for projectiles, and their functionality.
+]]
+
+Projectile =
+{
+    position,
+    direction,
+    speed,
+    size
+}
+
