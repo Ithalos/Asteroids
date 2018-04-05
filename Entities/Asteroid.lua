1ad04ec96e43930d357cd7932993a737ecb76d22
Sam Triest <sam.triest@gmail.com>
2018-04-06 00:42:04 +0200

    Load Asteroid images from disk

--- Entities/Asteroid.lua
+++ Entities/Asteroid.lua
@@ -22,3 +22,11 @@ Asteroid =
 ]]
 local asteroids = {}
 
+--[[
+    Load Asteroid images.
+]]
+local shapes = {}
+shapes[1] = love.graphics.newImage("Resources/Images/Asteroid_1.png")
+shapes[2] = love.graphics.newImage("Resources/Images/Asteroid_2.png")
+shapes[3] = love.graphics.newImage("Resources/Images/Asteroid_3.png")
+
