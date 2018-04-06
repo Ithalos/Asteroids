9a6cf5ea192126b277fff9675e77639151752d73
Sam Triest <sam.triest@gmail.com>
2018-04-07 00:43:57 +0200

    Add Asteroid constructor

--- Entities/Asteroid.lua
+++ Entities/Asteroid.lua
@@ -30,3 +30,28 @@ shapes[1] = love.graphics.newImage("Resources/Images/Asteroid_1.png")
 shapes[2] = love.graphics.newImage("Resources/Images/Asteroid_2.png")
 shapes[3] = love.graphics.newImage("Resources/Images/Asteroid_3.png")
 
+--[[
+    Creates a new Asteroid object and adds it to the asteroids table.
+]]
+function Asteroid:New(position)
+    newAsteroid = {}
+    setmetatable(newAsteroid, self)
+    self.__index = self
+
+    -- Select a random shape
+    newAsteroid.image = shapes[math.random(1, 3)]
+
+    newAsteroid.position = position
+    newAsteroid.direction = Vector2:New(math.random(-10, 10), math.random(-10, 10))
+    newAsteroid.direction:Normalise()
+    newAsteroid.rotation = math.random(-10, 10)
+    newAsteroid.speed = 100
+
+    newAsteroid.size = 1
+    newAsteroid.offset = newAsteroid.image:getWidth() / 2
+
+    newAsteroid.collisionRadius = 50
+
+    table.insert(asteroids, newAsteroid)
+end
+
