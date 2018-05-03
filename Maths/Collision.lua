396fc1dace4f7da23958c84e39a93c929597980a
Sam Triest <sam.triest@gmail.com>
2018-05-03 23:49:51 +0200

    Add function to check distances between points

--- Maths/Collision.lua
+++ Maths/Collision.lua
@@ -5,3 +5,12 @@
     Handles collisions between asteroids, projectiles and the player.
 ]]
 
+--[[
+    Checks whether two points are within a specified distance of each other.
+]]
+function InRange(p1, p2, distance)
+    -- This is not entirely accurate when using circles, but works for now
+    return math.abs(p1.position.x - p2.position.x) < distance and
+           math.abs(p1.position.y - p2.position.y) < distance
+end
+
