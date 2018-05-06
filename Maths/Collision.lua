aca381f5b2f33e8840fc4034de3c688320c33d40
Sam Triest <sam.triest@gmail.com>
2018-05-06 21:04:55 +0200

    Add function to detect collisions
    Currently, this function only checks for collisions between asteroids
    and projectiles, but may be updated to also detect player-asteroid
    collisions in the future.

--- Maths/Collision.lua
+++ Maths/Collision.lua
@@ -6,6 +6,35 @@
 ]]
 
 --[[
+    Iterates over the current asteroids and projectiles in the game, and checks
+    whether any projectiles are colliding with any asteroids.
+]]
+function DetectCollisions()
+    p = GetProjectiles()
+    a = GetAsteroids()
+
+    -- Return if we have no elements to iterate over
+    if #a < 1 and #p < 1 then
+        return
+    end
+
+    -- Iterate over every asteroid
+    for i = #a, 1, -1 do
+        -- Iterate over every projectile
+        for j = #p, 1, -1 do
+            -- Ensure the element exists
+            if a[i] ~= nil and p[j] ~= nil then
+                -- Check the distance between the current asteroid and projectile
+                if InRange(a[i], p[j], a[i].collisionRadius) then
+                    AsteroidHit(i)
+                    DeleteProjectileByIndex(j)
+                end
+            end
+        end
+    end
+end
+
+--[[
     Checks whether two points are within a specified distance of each other.
 ]]
 function InRange(p1, p2, distance)
