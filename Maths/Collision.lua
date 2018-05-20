536a430790825b36e1f00b8ed1fe87183bc7fc84
Sam Triest <sam.triest@pm.me>
2018-05-20 17:35:27 +0200

    Update InRange function to operate on vectors
    This function now operates on the vectors it receives as arguments directly,
    instead of having to retrieve those from the object passed in.

--- Maths/Collision.lua
+++ Maths/Collision.lua
@@ -25,7 +25,7 @@ function DetectCollisions()
             -- Ensure the element exists
             if a[i] ~= nil and p[j] ~= nil then
                 -- Check the distance between the current asteroid and projectile
-                if InRange(a[i], p[j], a[i].collisionRadius) then
+                if InRange(a[i].position, p[j].position, a[i].collisionRadius) then
                     player:IncreaseScore(a[i].size)
                     DeleteProjectileByIndex(j)
                     AsteroidHit(i)
@@ -36,13 +36,13 @@ function DetectCollisions()
 end
 
 --[[
-    Checks whether two points are within a specified distance of each other.
-    Calculate the Euclidean distance between them, and return true if that
-    distance is less than the distance specified, else false.
+    Checks whether two points (2D Vectors) are within a specified distance
+    of each other. Calculate the Euclidean distance between them, and return
+    true if that distance is less than the distance specified, else false.
 ]]
 function InRange(p1, p2, distance)
-    deltaX = math.abs(p1.position.x - p2.position.x)
-    deltaY = math.abs(p1.position.y - p2.position.y)
+    deltaX = math.abs(p1.x - p2.x)
+    deltaY = math.abs(p1.y - p2.y)
     deltaVec = Vector2:New(deltaX, deltaY)
 
     return deltaVec:Length() < distance
