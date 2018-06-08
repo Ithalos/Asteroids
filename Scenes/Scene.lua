755552df7c7be00e97739383c7d1024dabe1c9bc
Sam Triest <sam.triest@pm.me>
2018-06-08 16:57:25 +0200

    Add Scene data member and constructor

--- Scenes/Scene.lua
+++ Scenes/Scene.lua
@@ -5,3 +5,16 @@
     Handles the creation and usage of scenes.
 ]]
 
+Scene = {}
+
+--[[
+    Creates a Scene object that will be used to inherit functions from.
+]]
+function Scene:New()
+    scene = {}
+    setmetatable(scene, self)
+    self.__index = self
+
+    return scene
+end
+
