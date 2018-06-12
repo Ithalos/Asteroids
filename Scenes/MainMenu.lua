611f4033b3e44803fd21795511e35f47e5857006
Sam Triest <sam.triest@pm.me>
2018-06-12 16:34:27 +0200

    Render MainMenu scene menu options

--- Scenes/MainMenu.lua
+++ Scenes/MainMenu.lua
@@ -31,6 +31,19 @@ end
     Render the scene every frame.
 ]]
 function MainMenu:Render()
+    love.graphics.printf("Welcome to Asteroids!", 0, WINDOW_H / 4, WINDOW_W, "center")
+
+    r, g, b, a = love.graphics.getColor()
+    -- Iterate over the menu options and highlight the selected option in red
+    for i = 1, #options do
+        if options[i] == selected then
+            love.graphics.setColor(1, 0, 0, 1)
+        end
+        love.graphics.printf(options[i], 0, (WINDOW_H * 0.75) + (i * 50), WINDOW_W, "center")
+        -- Reset the colour
+        love.graphics.setColor(r, g, b, a)
+    end
+
     RenderAllAsteroids()
 end
 
