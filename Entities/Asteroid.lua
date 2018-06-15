f3f3d36869c7955e908d99aa436b669cfcbf92fe
Sam Triest <sam.triest@pm.me>
2018-06-15 19:28:49 +0200

    Update Asteroid shapes initialisation

--- Entities/Asteroid.lua
+++ Entities/Asteroid.lua
@@ -26,10 +26,12 @@ local asteroids = {}
 --[[
     Load Asteroid images.
 ]]
-local shapes = {}
-shapes[1] = love.graphics.newImage("Resources/Images/Asteroid_1.png")
-shapes[2] = love.graphics.newImage("Resources/Images/Asteroid_2.png")
-shapes[3] = love.graphics.newImage("Resources/Images/Asteroid_3.png")
+local shapes =
+{
+    love.graphics.newImage("Resources/Images/Asteroid_1.png"),
+    love.graphics.newImage("Resources/Images/Asteroid_2.png"),
+    love.graphics.newImage("Resources/Images/Asteroid_3.png")
+}
 
 --[[
     Creates a new Asteroid object and adds it to the asteroids table.
