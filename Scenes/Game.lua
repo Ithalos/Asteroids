b5543bf57ae795c23fa46c2f403167e42eaa13bd
Sam Triest <sam.triest@pm.me>
2018-06-18 16:23:40 +0200

    Add Game scene setup

--- Scenes/Game.lua
+++ Scenes/Game.lua
@@ -10,3 +10,31 @@ Game = Scene:New()
 -- Asteroid spawn locations
 local spawns
 
+--[[
+    Game setup.
+]]
+function Game:Init()
+    self.player = Player:New()
+
+    -- Assign the spawn locations after WINDOW_W and WINDOW_H have been initialised
+    spawns =
+    {
+        { 0           , 0            }, -- Top Left
+        { WINDOW_W / 2, 0            }, -- Top Middle
+        { WINDOW_W    , 0            }, -- Top Right
+        { WINDOW_W    , WINDOW_H / 2 }, -- Middle Right
+        { WINDOW_W    , WINDOW_H     }, -- Bottom Right
+        { WINDOW_W / 2, WINDOW_H     }, -- Bottom Middle
+        { 0           , WINDOW_H     }, -- Bottom Left
+        { 0           , WINDOW_H / 2 }  -- Middle Left
+    }
+
+    -- Number of initial asteroids to spawn
+    local asteroidCount = 10
+    for i = 1, asteroidCount do
+        -- Select a random location from the spawns table
+        local spawn = spawns[math.random(1, #spawns)]
+        Asteroid:New(spawn[1], spawn[2])
+    end
+end
+
