57391f03d23e432d46ffe659269d90dc4ee38fd4
Sam Triest <sam.triest@pm.me>
2018-07-02 20:39:34 +0200

    Display keybinds on main menu

--- Scenes/MainMenu.lua
+++ Scenes/MainMenu.lua
@@ -42,6 +42,13 @@ function MainMenu:Render()
         love.graphics.setColor(r, g, b, a)
     end
 
+    -- Display the controls in a smaller font
+    love.graphics.setFont(keysFont)
+    love.graphics.printf("Movement:     Arrow Keys",    50, 730, WINDOW_W, "left")
+    love.graphics.printf("Shoot:            Spacebar",  50, 760, WINDOW_W, "left")
+    love.graphics.printf("Hyperspace:   Left Control",  50, 790, WINDOW_W, "left")
+    love.graphics.setFont(font)
+
     RenderAllAsteroids()
 end
 
